194af0fa2f650c97990c9ae702824d2a5f82454c
Isreality <isrealitycreations@gmail.com>
2024-06-26 13:44:06 +0100

    input field

---
+++ src/Pages/Order.jsx
@@ -0,0 +1,57 @@
+import "../style.css";
+import Sidebar from "../Components/Sidebar";
+import Header from "../Components/Header";
+import Heading from "../Components/Heading";
+import { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
+// import Skeleton from 'react-loading-skeleton';
+
+const Order = () => {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+        setLoading(false)
+    }, 1000)
+  }, [])
+
+    return ( 
+        <div>
+
+          {loading ? (
+            <p>Loading...</p>
+          ) : (
+            <div className="flex flex-row">
+              {/* Sidebar */}
+              <div>
+                <Sidebar/>
+              </div>
+
+              {/* Header */}
+              <div className="w-full">
+                <div className="mb-4 items-center"><Header title="Orders" link="/order"/></div>
+                
+                <div className="px-8">
+                  <div className="mb-4"><Heading title="Orders"/></div>
+                </div>
+                
+                {/* Body */}
+                {/* Card */}
+                <div className="border border-disable rounded-md px-10 py-8 mx-8">
+                  
+                </div>
+
+                
+              </div>
+
+            </div>
+          )}
+
+         
+           
+        </div>
+     );
+}
+ 
+export default Order;
